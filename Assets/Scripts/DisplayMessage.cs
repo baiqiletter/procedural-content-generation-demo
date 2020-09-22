1fcbf61c81c511c635e96e7995ad96b7b0709b61
拜启letter <baiqiletter@gmail.com>
2020-09-23 00:37:35 +0800

    Update some assets and scripts

---
+++ Assets/Scripts/DisplayMessage.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayMessage : MonoBehaviour
+{
+    public GameObject messagePrefab;
+    public float stayTime = 1.5f;
+    public float distToTop = 200f;
+    public float verticalGap = 50f;
+
+    private bool isDisplaying;
+    private Queue<GameObject> messageQueue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isDisplaying = false;
+        messageQueue = new Queue<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int i = 0;
+        foreach(GameObject messageText in messageQueue)
+        {
+            //messageText.transform.position =
+            //    new Vector3(
+            //        messageText.transform.position.x,
+            //        -distToTop - i * verticalGap,
+            //        0);
+            messageText.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -distToTop - i * verticalGap);
+            i++;
+            //print(messageText.transform.position);
+        }
+    }
+
+    public void SendToMessage(string message)
+    {
+        //print("display message");
+        GameObject messageText = Instantiate(messagePrefab, transform.Find("Canvas"));
+        messageText.GetComponent<Text>().text = message;
+        messageText.GetComponent<RectTransform>().anchorMin = new Vector2(.5f, 1f);
+        messageText.GetComponent<RectTransform>().anchorMax = new Vector2(.5f, 1f);
+        messageQueue.Enqueue(messageText);
+        StartCoroutine(RemoveTopMessage());
+    }
+
+    IEnumerator RemoveTopMessage()
+    {
+        yield return new WaitForSeconds(stayTime);
+        Destroy(messageQueue.Dequeue());
+    }
+}
---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public float healthPercentageChange = 0f;
+    public float maxHealthChange = 0f;
+    public float attackChange = 0f;
+    public float defendChange = 0f;
+    public float rare = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //print("pick");
+            // change GameManager variables
+            GameManager.Instance.health += GameManager.Instance.maxHealth * healthPercentageChange;
+            GameManager.Instance.maxHealth += maxHealthChange;
+            GameManager.Instance.attack += attackChange;
+            GameManager.Instance.defend += defendChange;
+
+            // send message to game ui
+            if (healthPercentageChange != 0)
+            {
+                GameObject.Find("UI Controller").GetComponent<DisplayMessage>().SendToMessage("HP + " + GameManager.Instance.maxHealth * healthPercentageChange);
+            }
+            if (maxHealthChange != 0)
+            {
+                GameObject.Find("UI Controller").GetComponent<DisplayMessage>().SendToMessage("Max HP + " + maxHealthChange);
+            }
+            if (attackChange != 0)
+            {
+                GameObject.Find("UI Controller").GetComponent<DisplayMessage>().SendToMessage("Attack + " + attackChange);
+            }
+            if (defendChange != 0)
+            {
+                GameObject.Find("UI Controller").GetComponent<DisplayMessage>().SendToMessage("Defend + " + defendChange);
+            }
+
+            GameManager.Instance.itemNum++;
+            GameManager.Instance.score += GameManager.Instance.itemScore;
+
+            // destroy this pickup item
+            Destroy(gameObject);
+        }
+    }
+}
